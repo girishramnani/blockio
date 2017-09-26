8147d335b1e7c487b8b294dbd0476275b490b7d5
girishramnani <girishramnani95@gmail.com>
2017-09-26 21:17:55 +0530

    added some methods to join and info. also cleaned the utils

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var utils = require("../wallet_store/utils");
-var _ = require("lodash");
+var Client = require('bitcore-wallet-client');
+var _ = require('lodash');
+var url = require('url');
+var read = require('read')
+var log = require('npmlog');
+var Client = require('bitcore-wallet-client');
 var router = express.Router();
 var network = "testnet";
-var baseWalletLocation = "../wallets/"
+var baseWalletLocation = "../wallets/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json(
       {
         "/:name":"POST - create a new wallet with name",
+        "/:name/join":"POST - join the wallet",
+        "/:name/info":"GET to get the wallet",
         "/:name/create_address":"POST - create a new address",
         "/:name/send":"POST to send money to an address",
         "/:name/balance":"GET the balance of your wallet"
@@ -17,22 +24,20 @@ router.get('/', function(req, res, next) {
     )
 });
 
-function genOpts(walletName){
-    return {
-        "file": baseWalletLocation + walletName,
-    }
-} 
 function createWallet(walletName,copayerName,holders,min,cb) {
 
-    utils.getClient(genOpts(walletName), {
-        doNotComplete: true
-      }, function(client) {
+    utils.getClient(baseWalletLocation + walletName,{
+      doNotComplete: true
+    }, (client) => {
+
         client.createWallet(walletName, copayerName, holders, min, {
           network: network
         }, function(err, secret) {
-          cb({"error":err})
+          if (err != undefined) {
+            cb({"error":err})          
+          }
           console.log(' * ' + _.capitalize(network) + ' Wallet Created.');
-          utils.saveClient(genOpts(walletName), client, {
+          utils.saveClient(baseWalletLocation + walletName, client, {
             doNotOverwrite: true
           }, function() {
             if (secret) {
@@ -43,9 +48,32 @@ function createWallet(walletName,copayerName,holders,min,cb) {
             } 
           });
         });
+
       });
       
     
+}
+
+
+function joinWallet(copayerName,secret,cb) {
+
+    utils.getClient(baseWalletLocation + copayerName,{ doNotComplete: true }, (client) => {
+
+      
+    client.joinWallet(secret, copayerName, {}, function(err, wallet) {
+      if (err) cb({"error":err});
+      console.log(' * Wallet Joined.', wallet.name);
+      utils.saveClient(baseWalletLocation+copayerName,client,function(){});
+      cb({"wallet": wallet});
+    });
+  
+  });
+
+}
+function getWallet(walletName,cb) {
+  utils.getClient(baseWalletLocation + walletName,{
+    mustExist: true
+  },(client) => cb({"wallet":JSON.parse(client.export())}))
 }
 
 
@@ -53,10 +81,16 @@ router.post("/:name",
     function(req,res) {
     var walletName = req.params.walletName;
     var copayer = req.params.copayerName;
-    var holders = 2;
-    var minSign = 2;
+    var holders = req.params.holders;
+    var minSign = 1;
 
-    createWallet(walletName,copayer,holders,minSign,(data)=>res.json(data));
+    createWallet(walletName,copayer,minSign,holders,(data)=>res.json(data));
+})
+
+router.get("/:name/info",function(req,res) {
+
+  getWallet(req.params.name,(wallet) => res.json(wallet))
+
 })
 
 router.post("/:name/create_address",
@@ -65,5 +99,12 @@ router.post("/:name/create_address",
         
     }
 )
+
+// createWallet("GirishRamnani12","ravi123",1,4,(d)=>{console.log(d)})
+
+// joinWallet("ravi123456","WrbumuSVWKs1Cusmeg9npVKwcseR1GHHXyQ3MohXkuf8GiVRHsHfPU3rLzsS4Tm5qvQCGUVKyWTbtc",(d)=> console.log(d))
+
+getWallet("ravi1234",(d) => console.log(d));
+
 
 module.exports = router;
--- wallet_store/utils.js
+++ wallet_store/utils.js
@@ -4,39 +4,10 @@ var read = require('read')
 var log = require('npmlog');
 var Client = require('bitcore-wallet-client');
 var FileStorage = require('./filestorage');
-var sjcl = require('sjcl');
-var hostName = "http://35.192.209.96:3232";
-var WALLET_ENCRYPTION_OPTS = {
-  iter: 5000
-};
+var host = "http://35.202.171.134:3232";
 
 var Utils = function() {};
 
-var die = Utils.die = function(err) {
-  if (err) {
-    if (err.code && err.code == 'ECONNREFUSED') {
-      console.error('Could not connect to Bicore Wallet Service');
-    } else {
-      console.error(err);
-    }
-    process.exit(1);
-  }
-};
-
-Utils.parseMN = function(text) {
-  if (!text) throw new Error('No m-n parameter');
-
-  var regex = /^(\d+)(-|of|-of-)?(\d+)$/i;
-  var match = regex.exec(text.trim());
-
-  if (!match || match.length === 0) throw new Error('Invalid m-n parameter');
-
-  var m = parseInt(match[1]);
-  var n = parseInt(match[3]);
-  if (m > n) throw new Error('Invalid m-n parameter');
-
-  return [m, n];
-};
 
 
 Utils.shortID = function(id) {
@@ -49,18 +20,11 @@ Utils.confirmationId = function(copayer) {
 
 
 Utils.doLoad = function(client, doNotComplete, walletData, password, filename, cb) {
-  if (password) {
-    try {
-      walletData = sjcl.decrypt(password, walletData);
-    } catch (e) {
-      die('Could not open wallet. Wrong password.');
-    }
-  }
-
+  
   try {
     client.import(walletData);
   } catch (e) {
-    die('Corrupt wallet file.');
+    cb({"error":'Corrupted wallet'});
   };
   if (doNotComplete) return cb(client);
 
@@ -72,28 +36,14 @@ Utils.doLoad = function(client, doNotComplete, walletData, password, filename, c
   });
   client.openWallet(function(err, isComplete) {
     if (err) throw err;
-
     return cb(client);
   });
 };
 
-Utils.loadEncrypted = function(client, opts, walletData, filename, cb) {
-  read({
-    prompt: 'Enter password to decrypt:',
-    silent: true
-  }, function(er, password) {
-    if (er) die(err);
-    if (!password) die("no password given");
 
-    return Utils.doLoad(client, opts.doNotComplete, walletData, password, filename, cb);
-  });
-};
-
-Utils.getClient = function(args, opts, cb) {
+Utils.getClient = function(filename, opts, cb) {
   opts = opts || {};
 
-  var filename = args.file ;
-  var host = hostName || process.env['BWS_HOST'] || 'https://bws.bitpay.com/';
 
   var storage = new FileStorage({
     filename: filename,
@@ -108,30 +58,25 @@ Utils.getClient = function(args, opts, cb) {
     if (err) {
       if (err.code == 'ENOENT') {
         if (opts.mustExist) {
-          die('File "' + filename + '" not found.');
+          cb({"error":"user wallet doesnt exist"});
         }
       } else {
-        die(err);
+        cb({"error":err});
       }
     }
 
     if (walletData && opts.mustBeNew) {
-      die('File "' + filename + '" already exists.');
+      cb({"error":'wallet already exists use other name'});
     }
     if (!walletData) return cb(client);
-
     var json;
     try {
       json = JSON.parse(walletData);
     } catch (e) {
-      die('Invalid input file');
+      cb({"error":'Invalid wallet'});
     };
 
-    if (json.ct) {
-      Utils.loadEncrypted(client, opts, walletData, filename, cb);
-    } else {
       Utils.doLoad(client, opts.doNotComplete, walletData, null, filename, cb);
-    }
   });
 };
 
@@ -139,48 +84,22 @@ Utils.doSave = function(client, filename, password, cb) {
   var opts = {};
 
   var str = client.export();
-  if (password) {
-    str = sjcl.encrypt(password, str, WALLET_ENCRYPTION_OPTS);
-  }
 
   var storage = new FileStorage({
     filename: filename,
   });
 
   storage.save(str, function(err) {
-    die(err);
-    return cb();
+    cb({"error":err});
   });
 };
 
-Utils.saveEncrypted = function(client, filename, cb) {
-  read({
-    prompt: 'Enter password to encrypt:',
-    silent: true
-  }, function(er, password) {
-    if (er) Utils.die(err);
-    if (!password) Utils.die("no password given");
-    read({
-      prompt: 'Confirm password:',
-      silent: true
-    }, function(er, password2) {
-      if (er) Utils.die(err);
-      if (password != password2)
-        Utils.die("passwords were not equal");
 
-      Utils.doSave(client, filename, password, cb);
-    });
-  });
-};
-
-Utils.saveClient = function(args, client, opts, cb) {
+Utils.saveClient = function(filename, client, opts, cb) {
   if (_.isFunction(opts)) {
     cb = opts;
     opts = {};
-  }
-
-  var filename = args.file || process.env['WALLET_FILE'] || process.env['HOME'] + '/.wallet.dat';
-
+  }   
   var storage = new FileStorage({
     filename: filename,
   });
@@ -189,15 +108,11 @@ Utils.saveClient = function(args, client, opts, cb) {
 
   storage.exists(function(exists) {
     if (exists && opts.doNotOverwrite) {
-      console.log(' * File already exists! Please specify a new filename using the -f option.');
-      return cb();
+      return cb({"error":"username already taken"});
     }
 
-    if (args.password) {
-      Utils.saveEncrypted(client, filename, cb);
-    } else {
-      Utils.doSave(client, filename, null, cb);
-    };
+    Utils.doSave(client, filename, null, cb);
+    
   });
 };
 
@@ -246,15 +161,6 @@ Utils.parseAmount = function(text) {
   return amountSat;
 };
 
-Utils.configureCommander = function(program) {
-  program
-    .version('0.0.1')
-    .option('-f, --file <filename>', 'Wallet file')
-    .option('-h, --host <host>', 'Bitcore Wallet Service URL (eg: http://localhost:3001/copay/api')
-    .option('-v, --verbose', 'be verbose')
-
-  return program;
-};
 
 Utils.UNITS = {
   btc: {
